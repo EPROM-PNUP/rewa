99c57adac6e0554edc4f966b99b4313e20c78913
Wahyu Mahardika <wmhrdk@gmail.com>
2021-03-21 17:11:01 +0800

    Catkin Innit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/wmhrdk/Dev/rewa/src
+# Build directory: /home/wmhrdk/Dev/rewa/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("rewa_motion")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/wmhrdk/Dev/rewa/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/wmhrdk/Dev/ros_ws/devel;/opt/ros/noetic'.split(';') if '/home/wmhrdk/Dev/ros_ws/devel;/opt/ros/noetic' != '' else []
